5596cd09603ab1533b41f77d1a275621595145ee
siddharthsinghgithub <siddharth@sugoilabs.com>
2020-03-28 18:16:41 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ class App extends Component {
             count: 0
         }
     }
-
+    /**
+     * function which sets the count
+     */
     toggleCount = () => {
         this.setState({ count: ++this.state.count });
     }
 
+    /**
+     * function to create the columns in the given row
+     */
     returnSpanRow = (spanCount) => {
         let span = [];
         for (let i = 0; i < spanCount; i++) {
@@ -26,6 +31,9 @@ class App extends Component {
             </div>);
     }
 
+    /**
+     * function to create the total rows in the box
+     */
     returnDivRow = (divCount, spanCount) => {
         let div = [];
         for (let i = 0; i < divCount; i++) {
@@ -35,11 +43,20 @@ class App extends Component {
         return div;
     }
 
+    /**
+     * function to make the desired box 
+     */
     divMaker = () => {
+        
+        // base condition
+        // if the count is zero return just a div
         if (!this.state.count)
             return (<div className="row-border"></div>);
 
+        // get the divCount i.e no. of rows
         let divCount = parseInt(this.state.count / 2) + 1;
+
+        // get the spanCount i.e no. of columns
         let spanCount = parseInt((this.state.count + 1) / 2) + 1;
 
         return this.returnDivRow(divCount, spanCount);
